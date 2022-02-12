0ba5618be4d89df3dfd2c89a79408496c2ca1db0
gdlyeabkov <gdlyeabkov@gmail.com>
2022-02-12 23:22:07 +0300

    expand_my_page

--- app/src/main/java/softtrack/product/health/EditMyPageActivity.java
+++ app/src/main/java/softtrack/product/health/EditMyPageActivity.java
@@ -1,14 +1,33 @@
 package softtrack.product.health;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditMyPageActivity extends AppCompatActivity {
+
+    public Button editMyPageActivitityFooterCancelBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_my_page);
+        initialize();
     }
+
+    public void initialize() {
+        editMyPageActivitityFooterCancelBtn = findViewById(R.id.edit_my_page_activitity_footer_cancel_btn);
+        editMyPageActivitityFooterCancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(EditMyPageActivity.this, MainActivity.class);
+                EditMyPageActivity.this.startActivity(intent);
+            }
+        });
+    }
+
 }
